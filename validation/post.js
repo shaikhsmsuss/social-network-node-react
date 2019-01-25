14abbaee7075abdc8c086addd3bc9fe2584e4078
shaikhsmsuss <shaikh.shoeb96@gmail.com>
2019-01-25 16:28:23 +0530

    backend validation for post routes

---
+++ validation/post.js
@@ -0,0 +1,23 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text : '';
+
+    if(!Validator.isLength(data.text,{min:10 , max:300})){
+        errors.text = 'post must be between 10 and 300 char'
+    }
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = 'text field is required';
+  }
+
+  
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
