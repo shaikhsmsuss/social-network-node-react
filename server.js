92272d4bccf93d8c7803d4d683dfc2248e1dc297
shaikhsmsuss <shaikh.shoeb96@gmail.com>
2019-01-25 16:08:13 +0530

    http connectivity and routes of the application

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
-const profile =  require('./routes/api/profile')
- 
-//DB config
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-// connect to mongodb 
+// Connect to MongoDB
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
-app.get('/',(req,res)=>res.send('hello world'));
 
-//user routres
+// Passport middleware
+app.use(passport.initialize());
 
-app.use('/api/users',users);
-app.use('/api/profile',profile);
+// Passport Config
+require('./config/passport')(passport);
 
-const port= process.env.PORT || 8080;
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 8080;
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
