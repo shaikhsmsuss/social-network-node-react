8276ed3efaf427ce863b090ed4eb8b85b7c3afdf
shaikhsmsuss <shaikh.shoeb96@gmail.com>
2019-01-25 16:32:40 +0530

    all the routes for front end and main file

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import{BrowserRouter as Router, Route} from 'react-router-dom';
+import './App.css';
+import Register from './components/auth/Register';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <Route exact path = "/"/>
+        <div className="container">
+            <Route exact path="/register" component={Register} />
+            
+            
+          </div>
+      </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
